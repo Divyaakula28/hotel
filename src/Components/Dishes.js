311228948c6dcb61429d4b0f6df603c8deda57a4
Divyaakula28 <divya2000akula@gmail.com>
2023-01-18 09:40:24 +0530

    setup gh-pages

--- src/Components/Dishes.js
+++ src/Components/Dishes.js
@@ -1,14 +1,14 @@
 import uthappizza from './images/logo1.png';
-import zucchipakoda from '../images/zucchipakoda.png';
-import vadonut from '../images/vadonut.png';
-import elaicheesecake from '../images/elaicheesecake.png'
+import zucchipakoda from './images/zucchipakoda.png';
+import vadonut from './images/vadonut.png';
+import elaicheesecake from './images/elaicheesecake.png'
 
 export const DISHES =
     [
         {
         id: 0,
         name:'Uthappizza',
-        image:'../images/logo1.png',
+        image:'https://vaya.in/recipes/details/uthappam-recipe/',
         category: 'mains',
         label:'Hot',
         price:'4.99',
